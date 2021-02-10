b0d7225e58e608a7f7b65a353c62b0e91244a83b
soongjamm <kimstz0303@gmail.com>
2021-02-11 02:34:59 +0900

    내장 웹서버 이해

--- src/whiteship_spring_boot/src/main/java/com/soongjamm/springbootlecture/Application.java
+++ src/whiteship_spring_boot/src/main/java/com/soongjamm/springbootlecture/Application.java
@@ -1,29 +1,55 @@
 package com.soongjamm.springbootlecture;
 
-import me.soongjamm.Holoman;
-import me.soongjamm.HolomanProperties;
-import me.soongjamm.HolomonProperties;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("", "/");
+
+        /**
+         *  서블릿 추가
+         */
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey, Tomcat");
+                writer.println("</title></head>");
+                writer.println("<body><h1>Hello Tomcat</h1></body>");
+                writer.println("</html>");
+            }
+        };
+
+        /**
+         *  서블릿 톰캣에 등록
+         */
+        String servletName = "helloServlet";
+        tomcat.addServlet("", servletName, servlet);
+
+        /**
+         * 컨텍스트에 매핑 추가
+         * /hello 요청이 오면 'servletName' 으로 등록된 위의 서블릿을 보여준다.
+         */
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.getConnector();
+        tomcat.start();
+        tomcat.getServer().await();
+
     }
-
-
-//    @Bean
-//    public Holoman holoman() {
-//        Holoman holoman = new Holoman();
-//        holoman.setHowLong(100);
-//        holoman.setName("soongs");
-//        return holoman;
-//    }
 
 }
